66fc76cb574ec7675f204f368aef3c9a807e0c0e
michalwojewoda <mw.pryw@gmail.com>
2021-01-18 23:10:43 +0100

    bacics

---
+++ first.js
@@ -0,0 +1,38 @@
+// inline comment 
+
+/*
+Inline comment 
+multiline sss 
+*/
+var number = 5; 
+/* Data Types
+undefined
+null
+boolean,
+string,
+symbol
+number,
+object
+
+*/
+
+// How to declare variable:
+
+var myName = "Beau" // main way to set up variable 
+
+myName = 8 
+
+let ourName = "Antonio" // thats variable that is local
+
+const pi = 3.14 // variable that cannot change
+
+var a;
+var b = 2;
+
+a = 7; 
+
+b = a;
+console.log(a)
+
+
+
